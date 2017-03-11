228b6c145f75f81a94b9cf1f2fa8bdbbc9c8d950
nihitx <masnadhossain@live.com>
2017-03-11 17:58:13 +0200

    server files added

---
+++ server/main.js
@@ -0,0 +1,66 @@
+var fs = require("fs");
+const apiai = require('apiai');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const youtube = require('./youtube.js');
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(publicPath));
+
+const ai = apiai("");
+const alarm = apiai("");
+
+/* ai getting built here */
+app.post('/api', (req, res , err )=> {
+  var answer = req.body.text;
+  console.log(answer);
+  var request = ai.textRequest(answer , {
+      sessionId: '0503657881'
+  });
+  request.on('response', function(response) {
+       return res.send(response.result.fulfillment.speech);
+  });
+  request.on('error', function(error) {
+      return res.send(error);
+  });
+  request.end();
+});
+
+/* alarm getting built here */
+app.post('/alarm', (req, res , err )=> {
+  var alarmRequest = req.body.text;
+  var request = alarm.textRequest(alarmRequest , {
+      sessionId: '0503657881'
+  });
+  request.on('response', function(response) {
+       return res.send(response.result);
+  });
+  request.on('error', function(error) {
+      return res.send(error);
+  });
+  request.end();
+});
+
+
+/* youtube part here */
+app.get('/youtube', (req,res,next)=>{
+  var play = req.query.text ;
+  youtube.search_youtube(play, (err,result)=>{
+    if(err){
+      return res.end(err);
+    }else{
+      return res.end(JSON.stringify(result));
+    }
+ });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
---
+++ server/youtube.js
@@ -0,0 +1,66 @@
+const google = require ('googleapis');
+
+google.options ({ auth: '' });
+var youtube = google.youtube ('v3');
+
+// Search Youtube -- callback is called on each found item (or error)
+function search_youtube (query, callback) {
+  var searchParams = {
+    part:             'snippet',
+    type:             'video',
+    q:                query,
+    maxResults:       50,
+    order:            'date',
+    safeSearch:       'moderate',
+    videoEmbeddable:  true
+  };
+
+  youtube.search.list (searchParams, function (err, res) {
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    res.items.forEach (function (result) {
+      var video = {
+        id:            result.id.videoId,
+        urlShort:      'https://youtu.be/' + result.id.videoId,
+        urlLong:       'https://www.youtube.com/watch?v=' + result.id.videoId,
+        published:     result.snippet.publishedAt,
+        title:         result.snippet.title || '',
+        description:   result.snippet.description || '',
+        images:        result.snippet.thumbnails,
+        channelTitle:  result.snippet.channelTitle,
+        channelId:     result.snippet.channelId,
+        live:          result.snippet.liveBroadcastContent || ''
+      };
+
+      // When you don't need `duration` and `definition` you can skip the section
+      // below to save API credits, but don't forget the `callback (null, video);`
+      var listParams = {
+        part: 'contentDetails',
+        id: video.id
+      };
+
+      youtube.videos.list (listParams, function (err2, data) {
+        if (err2) {
+          callback (err2);
+          return;
+        }
+
+        if (data.items.length) {
+          data.items[0].contentDetails.duration.replace (/PT(\d+)M(\d+)S/, function (t, m, s) {
+            video.duration = (parseInt (m, 10) * 60) + parseInt (s, 10);
+          });
+
+          video.definition = data.items[0].contentDetails.definition;
+          callback (null, video);
+        }
+      });
+    });
+  });
+}
+
+module.exports = {
+  search_youtube
+}
